3c2f5fc1cd694ab2758398b246e0776a4768fba2
DegtyarevPaul <99130466+DegtyarevPaul@users.noreply.github.com>
2022-06-10 14:25:49 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,10 +1,21 @@
-// Copyright 2022 NNTU-CS
+// Copyright 2022 DegtyarevPaul
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ITEM* head;
+ITEM* tail;
+TPQueue::ITEM* create(const T& value) {
+ITEM* i = new ITEM;
+i->tim = value;
+i->next = nullptr;
+return i;
+    }
+  struct ITEM {
+    T tim;
+    ITEM* next;
+  };
 };
 
 struct SYM {
