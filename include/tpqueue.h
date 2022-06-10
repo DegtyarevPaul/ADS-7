ed2c5bdd7d6a4a722357d754da6e1946e4d96578
DegtyarevPaul <99130466+DegtyarevPaul@users.noreply.github.com>
2022-06-10 16:59:08 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,18 +4,49 @@
 
 template<typename T>
 class TPQueue {
-ITEM* head;
-ITEM* tail;
-TPQueue::ITEM* create(const T& value) {
-ITEM* i = new ITEM;
-i->tim = value;
-i->next = nullptr;
-return i;
-    }
+private:
   struct ITEM {
-    T tim;
+    T sym;
     ITEM* next;
   };
+  TPQueue::ITEM* create(const T& value) {
+    ITEM* p = new ITEM;
+    p->sym = value;
+    p->next = nullptr;
+    return p;
+  }
+  ITEM* head;
+public:
+  TPQueue() :head(nullptr) {}
+  T pop() {
+    ITEM* temp = head;
+    head = head->next;
+    return temp->sym;
+  }
+  void push(T value) {
+    if (head) {
+      ITEM* th = create(value);
+      if (value.prior > head->sym.prior) {
+        th->next = head;
+        head = th;
+        return;
+      }
+      ITEM* temp = head;
+      while (temp->next) {
+      if (value.prior > temp->next->sym.prior) {
+        th = create(value);
+        th->next = temp->next;
+        temp->next = th;
+        return;
+      }
+        temp = temp->next;
+    }
+    temp->next = create(value);
+    } else {
+      head = create(value);
+      return;
+    }
+  }
 };
 
 struct SYM {
